25b06ea8ef12ba645f13f1c6190b00ee7e42ecad
Kelia-S <112966217+Kelia-S@users.noreply.github.com>
2024-05-22 22:57:18 +0000

    Pending changes exported from your codespace

--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,7 +1,7 @@
-function shortIntroduction() {
-  // Complete this function's body and arguments
-  
+function addNumbers(a,b){
+	// This is the function's body. Write your code here.
+	return a + b
 }
 
-// Fill the gaps with your data in the correct order
-console.log(shortIntroduction(" ", " ", " "))
+//Do not change the code below
+console.log(addNumbers(3,4));
--- exercises/18-While_loop/app.js
+++ exercises/18-While_loop/app.js
@@ -1,12 +1,30 @@
-//fix this function:
-function startCounting() {
-	let counter = 100;
-	while (counter <= 100) {
-		counter--;
-		console.log(counter);
-	}
+function getColor(colorNumber = 0) {
 
-	return counter;
+	//make sure the parameter is a number and not a string by converting the value to int:
+	colorNumber = parseInt(colorNumber);
+	switch (colorNumber) {
+		case 1: return "red";
+
+		case 2: return "yellow";
+
+		case 3: return "blue";
+
+		case 4: return "green";
+
+		default: return "black";
+
+	}
 }
 
-startCounting();
+function getAllStudentColors() {
+
+	//your loop here
+	// let exampleColor = getColor(1);
+	for (let i = 0; i < 10; i++) {
+		let exampleColor = getColor(Math.floor(Math.random() * 4) + 1)
+		console.log(exampleColor);
+	}
+}
+
+//call the function below with the number of students in the class and print on the console
+getAllStudentColors();
